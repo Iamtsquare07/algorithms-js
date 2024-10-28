564dcba37b0e2b16f6f1beac5cfedfd29958b691
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2024-10-28 23:17:51 +0100

    Add files via upload

--- autoscroll.js
+++ autoscroll.js
@@ -1,13 +1,10 @@
-window.onmousemove = function(event){ 
- 
-    const b = event.clientY; 
-    h = window.innerHeight; 
-    //The code below displays the live Ypos value and the height of the viewport 
-      console.log(b + " " + h)
+window.onmousemove = function (event) {
+  const b = event.clientY;
+  h = window.innerHeight;
+  console.log(b + " " + h);
 
-    //Our threshold Ypos value is 500 
-    if (b > (500)) 
-    { 
-    window.scrollBy(0,12); 
-    } 
-  }; 
+  //Our threshold Ypos value is 500
+  if (b > 500) {
+    window.scrollBy(0, 12);
+  }
+};
